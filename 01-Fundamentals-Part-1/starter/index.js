327007cdaeee04a4374019da168974b1664a21ad
edddddr <endt6342@gmail.com>
2024-10-02 10:11:42 -0900

    BMI Calculator

--- 01-Fundamentals-Part-1/starter/index.js
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -11,7 +11,26 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
 
 TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
-TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+TEST DATA `122: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
 
 GOOD LUCK 😀
 */
+
+
+const marksWeights = 78;
+const marksHeight = 1.95;
+
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+const marksBMI = marksWeights/ (marksHeight * 2)
+const johnBMI = johnWeight / (johnHeight * 2)
+
+const markHigherBMI = marksBMI > johnBMI;
+
+if(markHigherBMI) {
+    alert("Marks have a higher BMI ")
+}else{
+    alert("John have a higher BMI") 
+}
+// mass / (height * height)
