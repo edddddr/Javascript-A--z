6e0bacc665d07f6dea913d948b299d6ea99fb362
edddddr <endt6342@gmail.com>
2024-10-14 17:23:37 -0900

    ES6 Classes

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,29 +1,81 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+///////////////////////////////////////
+// Coding Challenge #1
 
-// Data needed for first part of the section
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-};
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and '"Perisic"'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+// const gk = 'montots';
+// const gk2 = 'seots';
+// const fp = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const fp2 = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+// const players1 = [gk, ...fp];
+// console.log(players1);
+// console.log();
+// const players2 = [gk2, ...fp2];
+// console.log(players2);
+
+// console.log([...players1, ...players2]);
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// const tema1 = {}
+// const draw = {}
+// const tema2 = {}
+// console.log(players1Final);
+
+// const printGoals = function()
+
+// // Data needed for a later exercise
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greetName = greet('hello');
+
+const greet = greeting => name => console.log(`${greeting} ${name}`);
+greet('hello')('jhonas');
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Car = function (speed) {
   this.speed = `${speed} km/hr`;
 };
@@ -21,3 +21,34 @@ const Mercedes = new Car(3000);
 console.log(Mercedes);
 
 BMW.accelerate();
+*/
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀 */
+class RCarr {
+  constructor(mak, speed) {
+    this.mak = mak;
+    this.speed = speed;
+  }
+
+  get speedUs() {
+    return `The current speed is ${this.speed / 1.6} mi/h`;
+  }
+
+  set speedUs(speed) {
+    return (this._speed = speed * 1.6);
+  }
+}
+
+const BMW = new RCarr('BMW', 2000);
+
+console.log(BMW);
